0a630c3334b5c8778470c8d369cf29d81b9401fc
Mayur Bhagoji Ajiwale <mayur-bhagoji.ajiwale@capgemini.com>
2022-09-14 18:15:08 +0530

    Added unit test case using XUnit, Moq and Bogus.

---
+++ TAL.Task.Test/MockData/MockData.cs
@@ -0,0 +1,23 @@
+﻿using Bogus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TAL.Task.Domains.Entities;
+
+namespace TAL.Task.Test.MockData
+{
+    public static class MockData
+    {
+        public static List<Occupation> GetMockOccupations()
+        {
+            int occupationId = 1;
+            string occupationName = "mockOccupation" + occupationId;
+            int ratingId = 1;
+            var faker = new Faker<Occupation>()
+                .RuleFor(c => c.OccupationId, occupationId++)
+                .RuleFor(c => c.Name, occupationName)
+                .RuleFor(c => c.RatingId, ratingId);
+            return faker.Generate(5);
+        }
+    }
+}
---
+++ TAL.Task.Test/Services/OccupationServiceTest.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Moq;
+using System.Collections.Generic;
+using TAL.Task.Api.Infrastructure.AutoMapper;
+using TAL.Task.Api.Services;
+using TAL.Task.Domains.Entities;
+using TAL.Task.Repositories.Repositories;
+using Xunit;
+
+namespace TAL.Task.Test.Services
+{
+    public class OccupationServiceTest
+    {
+        private readonly OccupationService occupationService;
+        private Mock<IOccupationRepository> occupationRepositoryMock;
+        private IMapper mapper;
+
+        public OccupationServiceTest()
+        {
+            var mockMapper = new MapperConfiguration(cfg => cfg.AddProfile(new MappingProfile()));
+            mapper = mockMapper.CreateMapper();
+            occupationRepositoryMock = new Mock<IOccupationRepository>();
+            occupationService = new OccupationService(occupationRepositoryMock.Object, mapper);
+
+        }
+
+        [Fact]
+        public void GetOccupationsList_Shouldreturn_Occupations()
+        {
+            // Arrange
+             
+            List<Occupation> occupations = TAL.Task.Test.MockData.MockData.GetMockOccupations();             
+            occupationRepositoryMock.Setup(x => x.GetOccupationsList()).Returns(occupations);
+
+            // Act
+            var occupationsList = occupationService.GetOccupationsList();
+
+            // Assert,
+            Assert.NotNull(occupationsList);
+        }
+    }
+}
